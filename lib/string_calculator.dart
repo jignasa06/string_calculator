66b8284c456d3d32610b0ebe18204c606957c62d
jignasa <jignasasolanki95@gmail.com>
2025-09-03 19:41:04 +0530

    feat(step2): handle single number input (green)

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,6 +3,9 @@ library string_calculator;
 class Calculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
+    if (!numbers.contains(',') && !numbers.contains('\n')) {
+      return int.parse(numbers);
+    }
     return 0;
   }
 }
