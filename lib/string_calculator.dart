1a27a65a18d35c02590b07c1d1d4652ddbe440ec
jignasa <jignasasolanki95@gmail.com>
2025-09-03 19:55:56 +0530

    feat(step4): support newline and comma delimiters and return sum of input (green)

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,8 +3,8 @@ library string_calculator;
 class Calculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    final parts = numbers.split(',');
-    final ints = parts.map(int.parse).toList();
+    final parts = numbers.split(RegExp(r'[,\n]'));
+    final ints = parts.map(int.parse);
     return ints.fold(0, (a, b) => a + b);
   }
 }
