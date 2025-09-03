54aa636aeb06f7e847036d93efc1b9db2da012ee
jignasa <jignasasolanki95@gmail.com>
2025-09-03 19:50:52 +0530

    feat(step3): should return sum of comma-separated numbers (green)

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -3,9 +3,8 @@ library string_calculator;
 class Calculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    if (!numbers.contains(',') && !numbers.contains('\n')) {
-      return int.parse(numbers);
-    }
-    return 0;
+    final parts = numbers.split(',');
+    final ints = parts.map(int.parse).toList();
+    return ints.fold(0, (a, b) => a + b);
   }
 }
